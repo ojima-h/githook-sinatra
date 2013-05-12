55582ecdaba7254c6b5a51ae8d9f520da1811afe
kawasemi-project <kawasemi@www2237u.sakura.ne.jp>
2013-05-12 16:48:54 +0900

    Configuration for rack and thin

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'git'
 
 git = Git.open '/home/kawasemi/public_html', log: Logger.new('log/git.log')
 
+get '/' do
+  'hello'
+end
 post '/kawasemi' do
   git.remote(:origin).fetch
   git.reset_hard 'origin/master'
